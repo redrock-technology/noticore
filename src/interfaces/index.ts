313ef2292bab51a018cdaa554b92e82818ead788
behrad kazemi <behrad.kzm@gmail.com>
2025-11-06 18:18:51 +0330

    update index.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,7 @@ export * from './notification-entity.interface';
 export * from './message.interface';
 export * from './config.interface';
 export * from './entity.interface';
+export * from './mq.interface';
+export * from './resource-lock.interface';
+export * from './repository.interface';
+export * from './pagination.interface';
